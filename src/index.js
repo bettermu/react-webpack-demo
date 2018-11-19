6968fa6606db910ffb1502702fec049f2756ddf5
bettermu <1113783908@qq.com>
2018-11-19 09:42:17 +0800

    引入react-router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@
 //element.innerHTML = 'hello world!'
 
 import React from 'react'
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
 import App from './app.jsx'
 
-ReactDom.render(<App />,document.getElementById('root'))
+//ReactDom.render(<App />,document.getElementById('root'))
+
+import { HashRouter, Route, hashHistory, Switch } from 'react-router-dom'
+    
+    
+    
+const SliderComponent = () => (
+  <Switch>
+    <Route exact path='/' component={App} />
+  </Switch>
+)
+    
+ReactDOM.render((
+  <HashRouter history={hashHistory}>
+    <SliderComponent />
+  </HashRouter>
+), document.getElementById('root'));
