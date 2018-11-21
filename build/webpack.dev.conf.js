8f74a8a8993b8b2f897c28cc5f772aabb8c0e082
bettermu <1113783908@qq.com>
2018-11-21 14:20:58 +0800

    安装axios，配置开发环境proxy代理

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -31,6 +31,13 @@ module.exports = merge(baseWebpackConfig, {
     https: false,
     noInfo: true,
     open: true,
-    proxy: {}
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
   }
 })
